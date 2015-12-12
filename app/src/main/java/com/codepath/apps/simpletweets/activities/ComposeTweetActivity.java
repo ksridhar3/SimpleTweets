fb00f25a80f4d6469805a3cb5d1370bc6f54d164
ksridhar3 <ksridhar3@gmail.com>
2015-12-12 02:32:16 -0800

    Menu items for home time line activity added

---
+++ app/src/main/java/com/codepath/apps/simpletweets/activities/ComposeTweetActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.apps.simpletweets.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.apps.simpletweets.R;
+
+public class ComposeTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_tweet);
+    }
+}
